5c5276f5db19a27675ce55609c2358c870bcc161
Gambit <Gambit@Gambit>
2024-07-04 01:48:50 +0530

    second init

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ struct Particle {
     Vector2 pos{};
     float radius = 50.0f;
 };
-
+bool checkCollision (Particle &c1 ,Particle &c2);
 
 int main() {
 
@@ -17,19 +17,14 @@ int main() {
     auto clr = WHITE;
     while (!WindowShouldClose()) {
         c1 = {GetMousePosition()};
-        float dx = c2.pos.x - c1.pos.x;
-        float dy = c1.pos.y - c2.pos.x;
-        float distance = std::sqrt(dx*dx + dy*dy);
-        float sumofRadii = c1.radius  + c2.radius;
-        std::string distanceStr = "Distance:"+std::to_string(distance);
-        if (distance <= sumofRadii) {
+        if (checkCollision(c1 , c2)) {
             clr = SKYBLUE;
         }else {
             clr = WHITE;
         }
+
         ClearBackground(BLACK);
         BeginDrawing();
-        DrawText(distanceStr.c_str() , 50 , 0, 20 , WHITE);
         DrawCircleV(c1.pos , c1.radius , clr);
         DrawLineV(c1.pos , c2.pos, WHITE);
         DrawCircleV(c2.pos , c2.radius , WHITE);
@@ -38,4 +33,16 @@ int main() {
     CloseWindow();
     return 0;
 }
-
+bool checkCollision (Particle &c1 ,Particle &c2) {
+    float dx = c2.pos.x - c1.pos.x;
+    float dy = c1.pos.y - c2.pos.x;
+    float distance = std::sqrt(dx*dx + dy*dy);
+    float sumofRadii = c1.radius  + c2.radius;
+    std::string distanceStr = "Distance:"+std::to_string(distance);
+    DrawText(distanceStr.c_str() , 50 , 0, 20 , WHITE);
+    if (distance <= sumofRadii) {
+        return true;
+    }else {
+        return false;
+    }
+}
